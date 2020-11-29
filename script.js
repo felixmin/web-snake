7dd9f2c4709bfbe03a04ea4fe2b9ac37f939b803
felixmin <64661015+felixmin@users.noreply.github.com>
2020-11-29 22:23:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@
 
     var headingHeight = document.getElementById('heading').style.height;
 
-    document.getElementById("btns").style.height = screenHeight - ctx.canvas.height - headingHeight;
+    document.getElementById("btns").style.height = screenHeight - ctx.canvas.height - headingHeight + "px";
 
 
     /*******************************************************
